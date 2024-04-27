bf84bc45f15d942b4132da54bfe6f3b2467de776
Mahmoud-ILLOURMANE <illourmanemahmoud@hotmail.com>
2024-04-27 17:34:55 +0200

    Initialisation du projet avec la mise en place des packages gRpc.

---
+++ client/toolsClient.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+ *
+ *	Ce fichier contient toutes les fonctions utiles à la partie
+ *	client du projet.
+ *	Auteur: Mahmoud Illourmane
+ *	Date : 27/04/2024
+ *
+ */
+
+import (
+	"fmt"
+)
+
+/*==/////////// Public Functions  ///////////==*/
+
+func PrintHelloWorld() {
+	fmt.Println("Hello, World!")
+}
+
+/*==/////////// END/Public Functions  ///////////==*/
